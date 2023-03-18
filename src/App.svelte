a69880bf785b57b897f7b96e1c8d04fd5c464517
Karol Banat <karol.banat@gmail.com>
2023-03-18 16:27:52 +0100

    Done

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+  import Card from "./lib/Card.svelte";
+  import QRCode from './assets/image-qr-code.png'
+
+
 </script>
 
 <main>
+    <Card imageSrc={QRCode} imageAlt='Frontend Mentor Page QR Code' heading='Improve your front-end skills by building projects' text='Scan the QR code to visit Frontend Mentor and take your coding skills to the next level'/>
 </main>
 
 <style>
+    main {
+        padding-inline: 1.75rem;
+    }
 </style>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    export let imageSrc: string
+    export let imageAlt: string = ''
+    export let heading: string = ''
+    export let text: string = ''
+</script>
+
+<article class="card">
+    <img src={imageSrc} alt={imageAlt} class="card__image">
+    <h1 class="card__heading">{heading}</h1>
+    <p>{text}</p>
+</article>
+
+<style>
+
+.card {
+	padding: 1rem;
+	padding-block-end: 2.5rem;
+	max-width: 20rem;
+
+	text-align: center;
+	background-color: var(--clr-neutral-100);
+	border-radius: var(--borr-400);
+	filter: drop-shadow(0 1.5625rem 1.5625rem hsl(0deg 0% 0% / 0.05));
+}
+
+.card__image {
+	margin-bottom: 1.5rem;
+	border-radius: var(--borr-200);
+}
+
+.card__heading {
+	margin-bottom: 1rem;
+	font-size: var(--fz-700);
+	color: var(--clr-primary-400);
+	line-height: 1.1;
+}
+
+.card > p {
+	padding-inline: 1rem;
+}
+
+</style>
